8f6b53d81179ed00135fb392879bf08199639bba
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 17:22:14 +0900

    feat: kakao oauth 로그인 서비스 로직 추가

--- src/main/java/com/thanksd/server/controller/AuthController.java
+++ src/main/java/com/thanksd/server/controller/AuthController.java
@@ -9,7 +9,6 @@ import com.thanksd.server.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +33,8 @@ public class AuthController {
 
     @Operation(summary = "카카오 OAuth 로그인")
     @PostMapping("/kakao")
-    public ResponseEntity<?> loginKakao(@RequestBody @Valid KakaoLoginRequest request) {
+    public Response<?> loginKakao(@RequestBody @Valid KakaoLoginRequest request) {
         OAuthTokenResponse response = authService.kakaoOAuthLogin(request);
-        return Response.ofSuccess("OK", response)
+        return Response.ofSuccess("OK", response);
     }
 }
--- src/main/java/com/thanksd/server/repository/MemberRepository.java
+++ src/main/java/com/thanksd/server/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.thanksd.server.repository;
 import com.thanksd.server.domain.Member;
 import com.thanksd.server.domain.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -13,4 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndPlatform(String email, Platform platform);
 
     Optional<Member> findByPlatformAndPlatformId(Platform platform, String platformId);
+
+    @Query("select m.id from Member m where m.platform = :platform and m.platformId = :platformId")
+    Optional<Long> findIdByPlatformAndPlatformId(Platform platform, String platformId);
 }
