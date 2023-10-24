ff4ad35630b277cc6c43d7e2919f3db74e44d423
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 15:34:55 +0900

    feat: jwt token 발급 로직 추가

---
+++ src/main/java/com/thanksd/server/exception/unauthorized/TokenExpiredException.java
@@ -0,0 +1,8 @@
+package com.thanksd.server.exception.unauthorized;
+
+public class TokenExpiredException extends UnauthorizedException {
+
+    public TokenExpiredException() {
+        super("로그인 인증 유효기간이 만료되었습니다. 다시 로그인 해주세요.", 1011);
+    }
+}
