947bf960bf0deed9e8effa07dfed13aa21c5ea86
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 16:44:35 +0900

    feat: oauth 회원가입 컨트롤러 메서드 추가

---
+++ src/main/java/com/thanksd/server/dto/request/OAuthMemberSignUpRequest.java
@@ -0,0 +1,20 @@
+package com.thanksd.server.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@ToString
+public class OAuthMemberSignUpRequest {
+
+    private String email;
+
+    @NotBlank(message = "1005:공백일 수 없습니다.")
+    private String platform;
+
+    @NotBlank(message = "1005:공백일 수 없습니다.")
+    private String platformId;
+}
