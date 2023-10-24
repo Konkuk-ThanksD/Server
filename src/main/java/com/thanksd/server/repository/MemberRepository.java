681f7556013b9b061c7821e778231c891f141302
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 15:38:04 +0900

    feat: 이메일 로그인 서비스 로직 추가

--- src/main/java/com/thanksd/server/repository/MemberRepository.java
+++ src/main/java/com/thanksd/server/repository/MemberRepository.java
@@ -4,7 +4,11 @@ import com.thanksd.server.domain.Member;
 import com.thanksd.server.domain.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Boolean existsByEmailAndPlatform(String email, Platform platform);
+
+    Optional<Member> findByEmailAndPlatform(String email, Platform platform);
 }
--- src/main/java/com/thanksd/server/service/AuthService.java
+++ src/main/java/com/thanksd/server/service/AuthService.java
@@ -1,9 +1,41 @@
 package com.thanksd.server.service;
 
+import com.thanksd.server.domain.Member;
+import com.thanksd.server.domain.Platform;
+import com.thanksd.server.dto.request.AuthLoginRequest;
+import com.thanksd.server.dto.response.TokenResponse;
+import com.thanksd.server.exception.badrequest.PasswordMismatchException;
+import com.thanksd.server.exception.notfound.NotFoundMemberException;
+import com.thanksd.server.repository.MemberRepository;
+import com.thanksd.server.security.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    public TokenResponse login(AuthLoginRequest request) {
+        Member findMember = memberRepository.findByEmailAndPlatform(request.getEmail(), Platform.THANKSD)
+                .orElseThrow(NotFoundMemberException::new);
+        validatePassword(findMember, request.getPassword());
+
+        String token = issueToken(findMember);
+        return TokenResponse.from(findMember.getId(), token);
+    }
+
+    private String issueToken(final Member findMember) {
+        return jwtTokenProvider.createToken(findMember.getId());
+    }
+
+    private void validatePassword(final Member findMember, final String password) {
+        if (!passwordEncoder.matches(password, findMember.getPassword())) {
+            throw new PasswordMismatchException();
+        }
+    }
 }
