5bcc9fd41fac34f87cd33b0fd50885f2fe05e13c
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-24 15:21:55 +0900

    test: 이메일 회원가입 관련 테스트 코드 추가

---
+++ src/test/java/com/thanksd/server/ServiceTest.java
@@ -0,0 +1,18 @@
+package com.thanksd.server;
+
+import com.thanksd.server.support.DatabaseCleanerCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(DatabaseCleanerCallback.class)
+public @interface ServiceTest {
+}
