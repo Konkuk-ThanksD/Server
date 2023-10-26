d5bf191594c2df3e95be876aabb8e5afc813cb14
youngjunkim <kyj0703@konkuk.ac.kr>
2023-10-26 15:41:17 +0900

    style: 사용하지 않는 메서드 삭제

--- src/main/java/com/thanksd/server/repository/DiaryRepository.java
+++ src/main/java/com/thanksd/server/repository/DiaryRepository.java
@@ -1,18 +1,12 @@
 package com.thanksd.server.repository;
 
 import com.thanksd.server.domain.Diary;
-import com.thanksd.server.domain.Member;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-
-    @Query("select d from Diary d where d.member = :member")
-    Optional<List<Diary>> findAllByMember(@Param("member") Member member);
-//    @Modifying
-//    @Query("update Diary d SET d.content = :content, d.font = :font, d.image = :image WHERE d.diary_id = :diary_id")
-//    void update(@Param("diary_id") Long diaryId, @Param("content")String content, @Param("font")String font,@Param("image")String image);
+    /*
+    @Modifying
+    @Query("update Diary d SET d.content = :content, d.font = :font, d.image = :image WHERE d.diary_id = :diary_id")
+    void update(@Param("diary_id") Long diaryId, @Param("content")String content, @Param("font")String font,@Param("image")String image);
+    //*/
 }
