f5aaca1cb928800866e93e56427e6ceabe694c6d
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-11-03 15:23:39 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,31 @@
-#!/usr/bin/env bash
-
 REPOSITORY=/home/ubuntu/Server
-APP_NAME=Server
+PROJECT_NAME=server-0.0.1-SNAPSHOT.jar
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f $APP_NAME)
-echo "$CURRENT_PID"
+echo "> Build 파일 복사"
+sudo cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
-if [ -z $CURRENT_PID ]
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ];
 then
-  echo "> 실행중인 해당 애플리케이션이 없습니다. "
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  kill -9 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 어플리케이션 배포"
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-chmod +x $JAR_NAME
+echo "> JAR_NAME: $JAR_NAME"
 
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_NAME 에 실행권한 추가"
+
+sudo chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
