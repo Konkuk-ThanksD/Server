20e2e2aa225b0f13fdb6c950369d6e18823ee53b
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-11-04 00:10:07 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
 
-#echo "> $JAR_NAME 에 실행권한 추가"
-
+echo "> $JAR_NAME 에 실행권한 추가"
 sudo chmod +x $JAR_NAME
 #sudo chmod +x $REPOSITORY/nohup.out
 
