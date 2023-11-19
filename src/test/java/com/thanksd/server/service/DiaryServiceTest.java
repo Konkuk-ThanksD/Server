dbbbc2bc17f1e185002c2c57210bad5f82aa61eb
youngjunkim <kyj0703@konkuk.ac.kr>
2023-11-19 19:50:45 +0900

    test: 주별 일기 개수 반환 로직 관련 테스트 추가

--- src/test/java/com/thanksd/server/service/DiaryServiceTest.java
+++ src/test/java/com/thanksd/server/service/DiaryServiceTest.java
@@ -1,6 +1,11 @@
 package com.thanksd.server.service;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.thanksd.server.domain.Diary;
 import com.thanksd.server.domain.Member;
 import com.thanksd.server.dto.request.DiaryRequest;
@@ -8,25 +13,22 @@ import com.thanksd.server.dto.request.DiaryUpdateRequest;
 import com.thanksd.server.dto.response.DiaryDateResponse;
 import com.thanksd.server.dto.response.DiaryInfoListResponse;
 import com.thanksd.server.dto.response.DiaryResponse;
+import com.thanksd.server.dto.response.DiaryWeekCountResponse;
 import com.thanksd.server.exception.badrequest.InvalidDateException;
 import com.thanksd.server.exception.badrequest.MemberMismatchException;
 import com.thanksd.server.exception.notfound.NotFoundDiaryException;
 import com.thanksd.server.repository.DiaryRepository;
 import com.thanksd.server.repository.MemberRepository;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.time.LocalDate;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ServiceTest
 class DiaryServiceTest {
@@ -55,7 +57,7 @@ class DiaryServiceTest {
     @DisplayName("일기를 저장한다.")
     public void saveDiary() {
         //given
-        DiaryRequest diaryRequest = new DiaryRequest("content", "sans",imageUrl);
+        DiaryRequest diaryRequest = new DiaryRequest("content", "sans", imageUrl);
 
         //when
         Long diaryId = diaryService.saveDiary(diaryRequest, member.getId()).getId();
@@ -68,35 +70,37 @@ class DiaryServiceTest {
         assertThat(diaries.size()).isEqualTo(1);
         assertEquals(diaryRequest.getContent(), findDiary.getContent(), "저장된 일기 내용이 같아야 한다.");
         assertEquals(diaryRequest.getFont(), findDiary.getFont(), "저장된 일기 폰트가 같아야 한다.");
-        assertEquals("images/1/cc4342b5-126f-4c73-a0b7-f70ad0a58ea6_test.jpg", findDiary.getImage(), "저장된 일기 이미지가 같아야 한다.");
+        assertEquals("images/1/cc4342b5-126f-4c73-a0b7-f70ad0a58ea6_test.jpg", findDiary.getImage(),
+                "저장된 일기 이미지가 같아야 한다.");
     }
 
     @Test
     @DisplayName("일기를 수정한다.")
     public void updateDiary() {
         //given
-        DiaryRequest oldDiaryRequest = new DiaryRequest("oldContent", "sans",imageUrl);
+        DiaryRequest oldDiaryRequest = new DiaryRequest("oldContent", "sans", imageUrl);
         DiaryUpdateRequest newDiaryRequest = new DiaryUpdateRequest("newContent", "sans", imageUrl);
 
         //when
-        Long diaryId = diaryService.saveDiary(oldDiaryRequest,member.getId()).getId();
+        Long diaryId = diaryService.saveDiary(oldDiaryRequest, member.getId()).getId();
         diaryService.updateDiary(newDiaryRequest, member.getId(), diaryId);
 
         //then
         DiaryResponse findDiaryResponse = diaryService.findOne(member.getId(), diaryId);
         assertEquals(newDiaryRequest.getContent(), findDiaryResponse.getContent(), "수정된 일기 내용이 같아야 한다.");
         assertEquals(newDiaryRequest.getFont(), findDiaryResponse.getFont(), "수정된 일기 폰트가 같아야 한다.");
-        assertEquals("images/1/cc4342b5-126f-4c73-a0b7-f70ad0a58ea6_test.jpg", findDiaryResponse.getImage(), "수정된 일기 이미지가 같아야 한다.");
+        assertEquals("images/1/cc4342b5-126f-4c73-a0b7-f70ad0a58ea6_test.jpg", findDiaryResponse.getImage(),
+                "수정된 일기 이미지가 같아야 한다.");
     }
 
     @Test
     @DisplayName("일기를 삭제한다.")
     public void deleteDiary() {
         //given
-        DiaryRequest diaryRequest = new DiaryRequest("content", "sans",imageUrl);
+        DiaryRequest diaryRequest = new DiaryRequest("content", "sans", imageUrl);
 
         //when
-        Long diaryId = diaryService.saveDiary(diaryRequest,member.getId()).getId();
+        Long diaryId = diaryService.saveDiary(diaryRequest, member.getId()).getId();
 
         //then
         List<DiaryResponse> diaries = diaryService.findMemberDiaries(member.getId()).getDiaries();
@@ -111,7 +115,7 @@ class DiaryServiceTest {
     @DisplayName("일기 업데이트 시, 존재하지 않는 일기는 업데이트 될 수 없다.")
     public void notFoundDiaryWhenUpdate() {
         //given
-        DiaryRequest oldDiaryRequest = new DiaryRequest("oldContent", "sans",imageUrl);
+        DiaryRequest oldDiaryRequest = new DiaryRequest("oldContent", "sans", imageUrl);
         DiaryUpdateRequest newDiaryRequest = new DiaryUpdateRequest("newContent", "sans", imageUrl);
 
         //when
@@ -126,18 +130,19 @@ class DiaryServiceTest {
     @DisplayName("업데이트 시, 공백값이 들어온 경우 기존 값을 유지한다.")
     public void notChangeValueWhenBlankRequest() {
         //given
-        DiaryRequest oldDiaryRequest = new DiaryRequest("oldContent", "sans",imageUrl);
+        DiaryRequest oldDiaryRequest = new DiaryRequest("oldContent", "sans", imageUrl);
         DiaryUpdateRequest newDiaryRequest = new DiaryUpdateRequest("", "", "");
 
         //when
-        Long diaryId = diaryService.saveDiary(oldDiaryRequest,member.getId()).getId();
+        Long diaryId = diaryService.saveDiary(oldDiaryRequest, member.getId()).getId();
         diaryService.updateDiary(newDiaryRequest, member.getId(), diaryId);
 
         //then
         DiaryResponse findDiaryResponse = diaryService.findOne(member.getId(), diaryId);
         assertEquals(oldDiaryRequest.getContent(), findDiaryResponse.getContent(), "기존 일기 내용이 같아야 한다.");
         assertEquals(oldDiaryRequest.getFont(), findDiaryResponse.getFont(), "기존 일기 폰트가 같아야 한다.");
-        assertEquals("images/1/cc4342b5-126f-4c73-a0b7-f70ad0a58ea6_test.jpg", findDiaryResponse.getImage(), "기존 일기 이미지가 같아야 한다.");
+        assertEquals("images/1/cc4342b5-126f-4c73-a0b7-f70ad0a58ea6_test.jpg", findDiaryResponse.getImage(),
+                "기존 일기 이미지가 같아야 한다.");
     }
 
     @Test
@@ -153,7 +158,7 @@ class DiaryServiceTest {
         //then
         assertThatThrownBy(() -> diaryService.findOne(secondMember.getId(), diaryId))
                 .isInstanceOf(MemberMismatchException.class);
-        assertThatThrownBy(() -> diaryService.updateDiary(diaryUpdateRequest,secondMember.getId(), diaryId))
+        assertThatThrownBy(() -> diaryService.updateDiary(diaryUpdateRequest, secondMember.getId(), diaryId))
                 .isInstanceOf(MemberMismatchException.class);
         assertThatThrownBy(() -> diaryService.deleteDiary(secondMember.getId(), diaryId))
                 .isInstanceOf(MemberMismatchException.class);
@@ -178,8 +183,31 @@ class DiaryServiceTest {
     @Test
     @DisplayName("해당 날짜에 존재하는 일기가 없다면 빈 리스트를 반환한다")
     public void findDiaryByDate() {
-        DiaryInfoListResponse findDiaryInfoList = diaryService.findDiaryByDate(member.getId(), LocalDate.ofEpochDay(2023-01-12));
+        DiaryInfoListResponse findDiaryInfoList = diaryService.findDiaryByDate(member.getId(),
+                LocalDate.ofEpochDay(2023 - 01 - 12));
 
         assertThat(findDiaryInfoList.getDiaryList().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("해당 날짜가 포함된 주에 작성한 일기의 개수를 반환한다.")
+    void findDiaryCountByWeek(){
+        DiaryRequest diaryRequest = new DiaryRequest("content", "sans", imageUrl);
+        diaryService.saveDiary(diaryRequest, member.getId());
+        LocalDate today = LocalDate.now();
+        DayOfWeek dayOfWeek = today.getDayOfWeek();
+        DayOfWeek anotherDayOfWeek = DayOfWeek.SUNDAY;
+        if(dayOfWeek == DayOfWeek.SUNDAY){
+            anotherDayOfWeek = DayOfWeek.THURSDAY;
+        }
+
+        DiaryWeekCountResponse diaryCountByWeek = diaryService.findDiaryCountByWeek(member.getId(), today);
+
+        Map<String, Integer> weekCounts = diaryCountByWeek.getWeekCounts();
+        Integer weekCount = weekCounts.get(dayOfWeek.name());
+        Integer anotherWeekCount = weekCounts.get(anotherDayOfWeek.name());
+
+        assertEquals(1, weekCount, "오늘 작성된 일기는 1개여야 한다.");
+        assertEquals(0, anotherWeekCount, "다른 날 작성된 일기는 0개여야 한다.");
     }
 }
